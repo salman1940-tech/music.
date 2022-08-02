f4d07d5b309c421ee02aaf6cf7fc74a1a200fada
salman1940-tech <110387160+salman1940-tech@users.noreply.github.com>
2022-08-03 01:20:46 +0500

    Initialize toggle-responsive-nav

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="bg-[#DBDBDB] px-[12%] py-5 min-h-screen">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+@media (max-width: 800px) {
+  div{
+    padding: 1.25rem;
+  }
+}
+</style>
